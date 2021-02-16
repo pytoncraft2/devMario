a9762cd1ab1bd87c4d40b676266d0f0a37849614
Hennequin <pythoncraft2@gmail.com>
2021-02-16 02:45:49 +0100

    ca fonctionne !

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
-
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+
+  socket.on('saut', function() {
+   console.log('saut de malade'); 
   });
+
 });
+
 
 /*
 io.on('connection', (socket) => {
